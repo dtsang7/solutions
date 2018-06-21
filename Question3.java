c781956440d710afa64e0d03b78ed181c61faacf
Chipok D. Tsang <dtsang7@yahoo.com>
2018-06-20 23:09:17 -0400

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,23 @@
+  /*
+	Question 3:
+	changePossibilities(amount,amount): Your quirky boss collects rare, old coins. They found out you're a programmer and asked you to solve something they've been wondering for a long time. 
+	Write a function that, given an amount of money and an array of coin denominations, computes the number of ways to make the amount of money with coins of the available denominations. 
+	Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations: 
+	1¢, 1¢, 1¢, 1¢
+	1¢, 1¢, 2¢
+	1¢, 3¢
+	2¢, 2¢
+	*/
+	
+	public int changePossibilities(int amount, int[] denominations){
+		int[] dp = new int[amount+1];
+		dp[0] = 1;
+		for(int denom: denominations){
+			for(int i = 1; i <= amount; i++){
+				if(i >= denom){
+					dp[i] = dp[i] + dp[i - denom];
+				}
+			}
+		}
+		return dp[amount];
+	}
